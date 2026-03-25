f7bae05d76981b7c03c2c0aa0000d5aacd9133ec
Hyllari <isadora.barbosa29@escola.pr.gov.br>
2026-03-25 08:41:24 -0300

    Add files via upload

---
+++ aula8ex2.js
@@ -0,0 +1,34 @@
+/*
+Exercício 2 (vale nota)
+CRIAR UM "BANCO DE DADOS" DE ALGUMA DAS SEGUINTES CATEGORIAS:
+LIVROS, FILMES, SÉRIES, JOGOS, ANIMES.
+USANDO OBJETOS, COM PELO MENOS 10 ELEMENTOS.
+
+
+- Crie uma função para transformar a lista de objetos em um arquivo JSON
+- Crie uma função para ler o arquivo JSON
+- Crie uma função para mostrar os dados de maneira organizada no terminal
+- Crie uma função para adicionar um novo elemento na lista
+- Crie uma função para filtrar os dados por preço (exemplo: preço<100)
+- Crie uma função para aplicar com desconto em todos os produtos
+
+
+SEGUIR O MODELO ABAIXO:
+*/
+const modelo = {
+    titulo: "Tìtulo do Livro",
+    autor: "Autor",
+    ano: 9999,
+    paginas: 333,
+    sinopse: "Quando virou o envelope, com a mão trêmula, Harry viu um lacre de cera púrpura com um brasão; um leão, uma águia, um texugo e uma cobra circulando uma grande letra\nHarry Potter nunca havia ouvido falar de Hogwarts quando as cartas começaram a aparecer no capacho da Rua dos Alfeneiros, nº 4. Escritos a tinta verde-esmeralda em pergaminho amarelado com um lacre de cera púrpura, as cartas eram rapidamente confiscadas por seus pavorosos tio e tia. Então, no aniversário de onze anos de Harry, um gigante com olhos que luziam como besouros negros chamado Rúbeo Hagrid surge com notícias surpreendentes: Harry Potter é um bruxo e tem uma vaga na Escola de Magia e Bruxaria de Hogwarts. Uma incrível aventura está para começar!",
+    preco: 99.90
+}
+
+const livros = [
+    {
+        // livro 1
+    },
+    {
+        // livro2 
+    }
+]
